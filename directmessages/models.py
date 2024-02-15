26e5c6f9e5c48a4ddfa759be8f79adf866cd308c
Diego Berrios <diegus99@gmail.com>
2024-02-14 22:29:06 -0400

    Added basic models for chat application

--- directmessages/models.py
+++ directmessages/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from network.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Conversation(models.Model):
+    users = models.ManyToManyField(User, related_name='conversations')
+
+    def serialize(self, user):
+        return {
+            'id' : self.id,
+            'messages' : [ message.serialize(user) for message in self.messages.all()]
+        }
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    content = models.CharField(max_length=4000)
+    timestamp = models.DateTimeField(default=timezone.now)
+    seen = models.ManyToManyField(User, related_name='seen_messages')
+
+    def serialize(self, user):
+        return {
+            'id' : self.id,
+            'conversation_id' : self.conversation.id,
+            'sender' : self.sender.serialize(user),
+            'content' : self.content,
+            'timestamp' : self.timestamp,
+            'seen' : user in self.seen.all()
+        }
+
+
+
+
