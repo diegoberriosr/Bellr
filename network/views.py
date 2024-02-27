3c4f367a8ff44a42e9d8f7d12841a2ae69b9db5a
Diego Berrios <diegus99@gmail.com>
2024-02-27 19:32:21 -0400

    Fixed front page login bugs.

--- network/views.py
+++ network/views.py
@@ -14,6 +14,8 @@ from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 from .models import User, Post, Transmission, Notification, Code
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -764,11 +766,14 @@ def generate_code(request):
 
     if new_account:
         register_value = random.randint(100000, 999999)
+        html_message=render_to_string('network/confirmation.html', {'code': register_value})
+
         send_mail(
-        'Your confirmation code',
-        f'Your confirmation code is: {register_value}',
-        'settings.EMAIL_HOST_USER',
-        ['rberriosdiego@gmail.com'],
+        subject='Your confirmation code',
+        message='',
+        html_message=html_message,
+        from_email='settings.EMAIL_HOST_USER',
+        recipient_list=['rberriosdiego@gmail.com'],
         fail_silently=False
         )
 
