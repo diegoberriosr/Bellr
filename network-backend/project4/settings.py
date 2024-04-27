15bdd06ad621dd0c4d93331dafedce0776857f93
Diego Berrios <diegus99@gmail.com>
2024-04-26 21:22:59 -0400

    fixed some issues

--- network-backend/project4/settings.py
+++ network-backend/project4/settings.py
@@ -37,7 +37,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.render.com']
 
 
 # Application definition
