a591adbdd53c1d581c56ac1f1d4285f7d8ee5288
Diego Berrios <diegus99@gmail.com>
2024-04-29 19:02:00 -0400

    Fixed PostInteractions component (mode/theme bug)

--- network-frontend/src/components/General/PostInteractions.jsx
+++ network-frontend/src/components/General/PostInteractions.jsx
@@ -8,7 +8,7 @@ import { MdVerified } from "react-icons/md";
 import { MdClose } from 'react-icons/md';
 
 // Component imports
-import ClipLoader from "react-spinners/ClipLoader";
+import MoonLoader from "react-spinners/MoonLoader";
 
 // Context imports
 import GeneralContext from '../../context/GeneralContext';
@@ -26,7 +26,10 @@ const PostInteractions = ({ shrink, setShrink}) => {
   const { editedPost, filter, mode} = useContext(GeneralContext);
   const navigate = useNavigate();
 
-  const observer = useRef()
+  const observer = useRef();
+
+  const border = mode.background === 'white' ? 'border-black' : 'border-white';
+
   const lastProfile = useCallback( profile => {
     if (loading) return;
     if (observer.current) observer.current.disconnect();
@@ -36,7 +39,7 @@ const PostInteractions = ({ shrink, setShrink}) => {
       }
     })
     if (profile) observer.current.observe(profile);
-  })
+  });
 
   const handleFollow = (userId) => {
     let headers;
@@ -67,7 +70,7 @@ const PostInteractions = ({ shrink, setShrink}) => {
       })
     })
 
-  }
+  };
 
   useEffect( () => {
     setLoading(true);
@@ -99,50 +102,49 @@ const PostInteractions = ({ shrink, setShrink}) => {
       setLoading(false);
     })
 
-  }, [page])
+  }, [page]);
 
-  console.log(users);
 
   return (
     <div className={`w-screen h-screen sm:w-[500px] sm:h-[500px] ${shrink ? 'animate-shrink' : 'animate-grow'} bg-${mode.background} ${mode.text} sm:rounded-xl`}>
-        <header className='sticky top-0 h-12 p-5 z-10 transform'>
-                    <MdClose className='text-2xl text-white mt-1 cursor-pointer' onClick={() => {setShrink(true)}}/>
+        <header className={`sticky top-0 h-12 p-5 z-10 transform ${mode.text}`}>
+                    <MdClose className='text-2xl mt-1 cursor-pointer' onClick={() => {setShrink(true)}}/>
                     <h3 className='absolute top-4 ml-[40%] text-2xl font-bold'>{filter}</h3>
         </header>
         <main className='relative h-[calc(100vh - 49px)] overflow-y-auto'>
         { users && users.length > 0 && 
             users.map((profile, index) => {
             if (index === users.length - 1) {
-              return ( <div key={index} ref={lastProfile} className='w-full flex items-start p-2.5 pt-4 animate-grow text-white'>
-              <div className='w-10 h-10 rounded-full overflow-hidden ml-2.5'>
+              return ( <div key={index} ref={lastProfile} className={`w-full flex items-start p-2.5 pt-4 animate-grow ${mode.text}`}>
+              <div className='min-w-10 min-h-10 max-w-10 max-h-10 rounded-full overflow-hidden ml-2.5'>
                  <img src={profile.transmitter ? profile.transmitter.pfp : profile.pfp} alt='user pfp' className='object-cover w-full h-full' />
               </div>
               <div className='relative ml-2.5 w-full'>
-                { (user ? user.user_id : -1) !== (profile.transmitter ? profile.transmitter.user_id : profile.user_id) && <button type='button' className={`${profile.followed ? 'hover:text-red-900 hover:bg-red-900 hover:border-red-900 hover:bg-opacity-30' : `hover:bg-white hover:text-black`} text-white absolute top-1.5 right-1 w-[99px] h-[30px] border border-white rounded-full font-bold transition-colors duration-300`} onClick={() => { handleFollow(profile.transmitter ? profile.transmitter.user_id : profile.user_id)}}>{(profile.transmitter ? profile.transmitter.followed : profile.followed) ? 'Unfollow' : 'Follow'}</button>}
-                <p className='inline-flex items-center font-bold hover:underline' onClick={()=>{navigate(`/user/${profile.transmitter ? profile.transmitter.username : profile.username}`)}}>{ profile.transmitter ? profile.transmitter.profilename : profile.profilename} { (profile.transmitter ? profile.transmitter.verified : profile.verified) && <MdVerified className='text-twitter-blue ml-0.5'/>}</p>
+                { (user ? user.user_id : -1) !== (profile.transmitter ? profile.transmitter.user_id : profile.user_id) && <button type='button' className={`${profile.followed ? 'hover:text-red-900 hover:bg-red-900 hover:border-red-900 hover:bg-opacity-30' : `hover:bg-white hover:text-black`} ${mode.text} border ${border} absolute top-1.5 right-1 w-[99px] h-[30px] rounded-full font-bold transition-colors duration-300`} onClick={() => { handleFollow(profile.transmitter ? profile.transmitter.user_id : profile.user_id)}}>{(profile.transmitter ? profile.transmitter.followed : profile.followed) ? 'Unfollow' : 'Follow'}</button>}
+                <p className='inline-flex items-center font-bold cursor-pointer hover:underline' onClick={()=>{ setShrink(true); navigate(`/user/${profile.transmitter ? profile.transmitter.username : profile.username}`)}}>{ profile.transmitter ? profile.transmitter.profilename : profile.profilename} { (profile.transmitter ? profile.transmitter.verified : profile.verified) && <MdVerified className='text-twitter-blue ml-0.5'/>}</p>
                 <p className='text-gray-600'>@{profile.transmitter ? profile.transmitter.username : profile.username}</p>
                 <p className='w-full text-gray-600'>{profile.transmitter ? profile.transmitter.bio : profile.bio}</p>
               </div>
               </div>);
             };
-           return <div key={index} className='w-full flex items-start p-2.5 pt-4 animate-grow text-white'>
-              <div className='w-10 h-10 rounded-full overflow-hidden ml-2.5'>
+           return <div key={index} className={`w-full flex items-start p-2.5 pt-4 animate-grow`}>
+              <div className='min-w-10 min-h-10 max-w-10 max-h-10 rounded-full overflow-hidden ml-2.5'>
                  <img src={profile.transmitter ? profile.transmitter.pfp : profile.pfp} alt='user pfp' className='object-cover w-full h-full' />
               </div>
               <div className='relative ml-2.5 w-full'>
-                { (user ? user.user_id : -1) !== (profile.transmitter ? profile.transmitter.user_id : profile.user_id) && <button type='button' className={`${profile.followed ? 'hover:text-red-900 hover:bg-red-900 hover:border-red-900 hover:bg-opacity-30' : `hover:bg-white hover:text-black`} text-white absolute top-1.5 right-1 w-[99px] h-[30px] border border-white rounded-full font-bold transition-colors duration-300`} onClick={() => { handleFollow(profile.transmitter ? profile.transmitter.user_id : profile.user_id)}}>{(profile.transmitter ? profile.transmitter.followed : profile.followed) ? 'Unfollow' : 'Follow'}</button>}
-                <p className='inline-flex items-center font-bold hover:underline' onClick={()=>{navigate(`/user/${profile.transmitter ? profile.transmitter.username : profile.username}`)}}>{ profile.transmitter ? profile.transmitter.profilename : profile.profilename} { (profile.transmitter ? profile.transmitter.verified : profile.verified) && <MdVerified className='text-twitter-blue ml-0.5'/>}</p>
+                { (user ? user.user_id : -1) !== (profile.transmitter ? profile.transmitter.user_id : profile.user_id) && <button type='button' className={`${profile.followed ? 'hover:text-red-900 hover:bg-red-900 hover:border-red-900 hover:bg-opacity-30' : `hover:bg-white hover:text-black`}  ${mode.text} border ${border} absolute top-1.5 right-1 w-[99px] h-[30px] border rounded-full font-bold transition-colors duration-300`} onClick={() => { handleFollow(profile.transmitter ? profile.transmitter.user_id : profile.user_id)}}>{(profile.transmitter ? profile.transmitter.followed : profile.followed) ? 'Unfollow' : 'Follow'}</button>}
+                <p className='inline-flex items-center font-bold hover:underline' onClick={()=>{ setShrink(true); navigate(`/user/${profile.transmitter ? profile.transmitter.username : profile.username}`)}}>{ profile.transmitter ? profile.transmitter.profilename : profile.profilename} { (profile.transmitter ? profile.transmitter.verified : profile.verified) && <MdVerified className='text-twitter-blue ml-0.5'/>}</p>
                 <p className='text-gray-600'>@{profile.transmitter ? profile.transmitter.username : profile.username}</p>
                 <p className='w-full text-gray-600'>{profile.transmitter ? profile.transmitter.bio : profile.bio}</p>
               </div>
             </div>})
         }
+        </main>
         { loading &&      
-        <div className='absolute inset-0 h-full w-full flex justify-center items-center'>
-               <ClipLoader color={'#1D9BF0'} loading={loading} size={150} aria-label='Loading spinner' data-testid='loader'/> 
+        <div className='mt-5 w-full flex justify-center'>
+               <MoonLoader color={'#1D9BF0'} loading={loading} size={75} aria-label='Loading spinner' data-testid='loader'/> 
         </div>
         }
-        </main>
     </div>
   )
 }
