930bb299f8bb3d2d5da04c7b5fde1d53ac9a809b
Diego Berrios <diegus99@gmail.com>
2024-04-29 21:52:21 -0400

    Added more loading states

--- network-frontend/src/components/General/Searchbar.jsx
+++ network-frontend/src/components/General/Searchbar.jsx
@@ -1,24 +1,38 @@
 import { useState, useRef, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
 // Icon imports
 import { IoIosSearch } from "react-icons/io";
 import { MdVerified } from 'react-icons/md';
+
+// Component imports
+import BarLoader from 'react-spinners/BarLoader';
+
 // Context imports
 import GeneralContext from '../../context/GeneralContext';
 
 const Searchbar = () => {
   
   const [ isSearching, setIsSearching ] = useState(false);
-  const [ matches, setMatches] = useState([]); // TODO : <---- FIX
+  const [ matches, setMatches] = useState([]);
+  const [ loading, setLoading] = useState(false);
 
   const { mode } = useContext(GeneralContext);
   const text = useRef();
   const navigate = useNavigate();
-  
+  let barColor = '#1D9BF0';
+
+  if (mode.color === 'twitter-pink') barColor = '#F6187F' ;
+  if (mode.color === 'twitter-purple') barColor = '#7557FF' ;
+  if (mode.color === 'twitter-yellow') barColor = '#FCD500';
+  if (mode.color === 'twitter-orange') barColor = '#FC7A00';
+  if (mode.color === 'twitter-green') barColor = '#00B978';
+
   const handleOnChange = (event) => {
     if (event.target.value !== '')
     {
+      setLoading(true);
       axios({
         url : `https://bellr.onrender.com/search`,
         method : 'GET',
@@ -26,8 +40,13 @@ const Searchbar = () => {
       })
       .then ( res => {
         setMatches(res.data)
+        setLoading(false);
         return;
       })
+      .catch( err => {
+        console.log(err);
+        setLoading(false);
+      });
     }
     setMatches([])
  
@@ -40,6 +59,9 @@ const Searchbar = () => {
             <input ref={text} className='ml-4 bg-transparent text-sm w-full focus:outline-none' placeholder='Search' onFocus={() => { setIsSearching(true) }} onBlur={() => {setIsSearching(false)}} onChange={handleOnChange}/>
         </div>
         {isSearching && <div className={`absolute top-10 max-h-72 overflow-y-auto flex flex-col w-full shadow-custom rounded-xl bg-${mode.background}`}>
+            <div className='absolute w-full inset-0'>
+              { loading && <BarLoader color={barColor} width='100%'/>}
+            </div>
             { matches.length > 0 ? matches.map(user => <div key={user.user_id} className={`w-full flex items-center cursor-pointer hover:${mode.highlight} p-3 rounded-xl`} onMouseDown={() => {
                navigate(`/user/${user.username}`)}}>
                 <div classNam='w-10 h-10 overflow-hidden rounded-full overflow-hidden'>
@@ -51,7 +73,8 @@ const Searchbar = () => {
                 </div>
                </div>) : 
                <div className='h-20 w-full flex justify-center pt-5 text-gray-600 font-bold'>
-                  {text.current.value === ''  ? 'Try searching for people' : 'No matches found'}
+                  {text.current.value === ''  && 'Try searching for people'}
+                  { matches.length === 0 && !loading && text.current.value !== '' && 'No matches found'}
                 </div>
                }    
         </div>}
